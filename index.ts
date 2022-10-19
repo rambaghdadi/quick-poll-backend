cd9a5a0e49681d2dd7c427b3e351c55437642f3c
Ram Baghdadi <ram_baghdadi@hotmail.com>
2022-10-19 21:55:18 +0100

    added cors

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config"
 import express, { Express } from "express"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 import pollRoutes from "./routes/Poll.js"
 import optionRoutes from "./routes/Option.js"
@@ -11,18 +12,19 @@ const port = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 app.set("trust proxy", true)
 
-app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "https://quickpolls.vercel.app/")
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
-	)
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	res.setHeader("Access-Control-Allow-Credentials", "true")
-	next()
-})
+// app.use((req, res, next) => {
+// 	res.setHeader("Access-Control-Allow-Origin", "https://quickpolls.vercel.app/")
+// 	res.setHeader(
+// 		"Access-Control-Allow-Methods",
+// 		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
+// 	)
+// 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+// 	res.setHeader("Access-Control-Allow-Credentials", "true")
+// 	next()
+// })
 
 app.use(helmet())
 app.use("/api", pollRoutes)
