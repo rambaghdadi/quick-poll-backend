78ba1f060629c33a973b0234bacc0ea8b9fe959c
Ram Baghdadi <ram_baghdadi@hotmail.com>
2022-10-19 23:18:03 +0100

    removed credentials header

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
 	)
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	res.setHeader("Access-Control-Allow-Credentials", "true")
+	// res.setHeader("Access-Control-Allow-Credentials", "true")
 	next()
 })
 app.use(helmet())
