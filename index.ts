2416015053c7f9beb093d43d0db51ae098bf8dbc
Ram Baghdadi <ram_baghdadi@hotmail.com>
2022-10-19 22:37:10 +0100

    added manual cors

--- index.ts
+++ index.ts
@@ -13,23 +13,22 @@ const port = process.env.PORT
 // app.set("trust proxy", true)
 app.use(express.json())
 app.use(cookieParser())
-app.use(
-	cors({
-		origin: ["https://quickpolls.vercel.app"],
-	})
-)
+// app.use(
+// 	cors({
+// 		origin: ["https://quickpolls.vercel.app"],
+// 	})
+// )
 app.use(helmet())
-
-// app.use((req, res, next) => {
-// 	res.setHeader("Access-Control-Allow-Origin", "https://quickpolls.vercel.app/")
-// 	res.setHeader(
-// 		"Access-Control-Allow-Methods",
-// 		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
-// 	)
-// 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-// 	res.setHeader("Access-Control-Allow-Credentials", "true")
-// 	next()
-// })
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "https://quickpolls.vercel.app")
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
+	)
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	res.setHeader("Access-Control-Allow-Credentials", "true")
+	next()
+})
 
 app.use("/api", pollRoutes)
 app.use("/api", optionRoutes)
