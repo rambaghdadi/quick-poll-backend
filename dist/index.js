fe9d8d649628270339a15c717f2ab9333437df7c
Ram Baghdadi <ram_baghdadi@hotmail.com>
2022-10-20 12:23:55 +0100

    removed cors npm

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import pollRoutes from "./routes/Poll.js";
 import optionRoutes from "./routes/Option.js";
 import helmet from "helmet";
@@ -10,25 +9,21 @@ var port = process.env.PORT;
 // app.set("trust proxy", true)
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    origin: true,
-    preflightContinue: true,
-    credentials: true,
-}));
-// app.use((req, res, next) => {
-// 	res.setHeader("Access-Control-Allow-Origin", "*")
-// 	res.setHeader(
-// 		"Access-Control-Allow-Methods",
-// 		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
-// 	)
-// 	res.setHeader(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Authorization, Accept"
-// 	)
-// 	// res.setHeader("Access-Control-Allow-Credentials", "true")
-// 	next()
-// })
+// app.use(
+// 	cors({
+// 		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+// 		origin: true,
+// 		preflightContinue: true,
+// 		credentials: true,
+// 	})
+// )
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+});
 app.use(helmet());
 app.use("/api", pollRoutes);
 app.use("/api", optionRoutes);
--- index.ts
+++ index.ts
@@ -13,28 +13,29 @@ const port = process.env.PORT
 // app.set("trust proxy", true)
 app.use(express.json())
 app.use(cookieParser())
-app.use(
-	cors({
-		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-		origin: true,
-		preflightContinue: true,
-		credentials: true,
-	})
-)
+// app.use(
+// 	cors({
+// 		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+// 		origin: true,
+// 		preflightContinue: true,
+// 		credentials: true,
+// 	})
+// )
 
-// app.use((req, res, next) => {
-// 	res.setHeader("Access-Control-Allow-Origin", "*")
-// 	res.setHeader(
-// 		"Access-Control-Allow-Methods",
-// 		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
-// 	)
-// 	res.setHeader(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Authorization, Accept"
-// 	)
-// 	// res.setHeader("Access-Control-Allow-Credentials", "true")
-// 	next()
-// })
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
+	)
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Authorization, Accept"
+	)
+	res.setHeader("Access-Control-Allow-Credentials", "true")
+	next()
+})
+
 app.use(helmet())
 
 app.use("/api", pollRoutes)
